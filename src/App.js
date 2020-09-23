fefef17ad44a9c99b8dc7da2a1791141efedcb35
Yogev Henig <yogevhenig@gmail.com>
2020-09-23 17:01:28 +0300

    with telaviv and newyork

--- src/App.js
+++ src/App.js
@@ -7,30 +7,84 @@ import ReactHtmlParser from 'react-html-parser';
 
 
 class App extends Component {
-  // constructor(){
-  //   super();
-  //   //this.state = initialState;
-  // }
+  constructor(){
+    super();
+    this.state = {
+      name1: 'Tel-Aviv',
+      name2: 'New-York',
+      city1 : {
+        temperature: '',
+        Humidity: '',
+        Windspeed: '',
+        icon: '',
+      },
+      city2 : {
+        temperature: '',
+        Humidity: '',
+        Windspeed: '',
+        icon: ''
+      }
+    };
+  }
+
+  fetchDataByCity = (city, index) => {
+    var proxyUrl = 'https://cors-anywhere.herokuapp.com/',
+        targetUrl = `https://www.google.com/search?q=weather+${city}`
+    fetch(proxyUrl + targetUrl)
+    .then(res => res.text())
+    .then(data => {
+      data = data.substr(data.indexOf('Weather Result'))
+      let temperatureIndex = data.indexOf('id="wob_ttm" style="display:none">')
+      let temperature = data.substr(temperatureIndex + 'id="wob_ttm" style="display:none">'.length,2)
+      let humidityIndex = data.indexOf('<span id="wob_hm">')
+      let humidity = data.substr(humidityIndex + '<span id="wob_hm">'.length,2)
+      let windspeedIndex = data.indexOf('d="wob_tws">')
+      let windspeed = data.substr(windspeedIndex + 'd="wob_tws">'.length,2)
+
+      let iconUrlStartIndex = data.indexOf('gstatic');
+      let iconUrlEndIndex = iconUrlStartIndex
+      while (data[iconUrlEndIndex] + data[iconUrlEndIndex+1] + data[iconUrlEndIndex+2] !== 'png'){
+        iconUrlEndIndex++;
+      }
+
+      let iconURL = data.substr(iconUrlStartIndex, iconUrlEndIndex - iconUrlStartIndex + 3);
+      console.log(iconURL)
+      if (index === 1){
+        this.setState(
+            {
+              city1 : {
+                temperature: temperature,
+                humidity: humidity,
+                windspeed: windspeed,
+                icon: iconURL
+              }
+            }
+        )
+      } else {
+        this.setState(
+            {
+              city2 : {
+                temperature: temperature,
+                humidity: humidity,
+                windspeed: windspeed,
+                icon: iconURL
+              }
+            }
+        )
+      }
+    })
+  }
 
   componentDidMount() {
-    //
-    // var proxyUrl = 'https://cors-anywhere.herokuapp.com/',
-    //     targetUrl = 'https://www.google.com/search?q=weather+new-york'
-    // fetch(proxyUrl + targetUrl)
-
-
-
-     let bla = fetch('https://www.google.com/search?q=weather+new-york')
-    //   headers: { mode: 'no-cors' }
-		// 	})
-      .then((res) => console.log())
-    //let bla = ReactHtmlParser()
-    //https://www.google.com/search?q=weather+new-york
-    console.log('hiii')
+    this.fetchDataByCity(this.state.name1,1);
+    this.fetchDataByCity(this.state.name2,2);
+    // console.log('heeeelo',this.state.city1.icon)
+    // console.log(this.state.city2.icon)
   }
 
   render() {
     return (
+
       <div className="App">
         <header className="App-header">
           <img src={'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRfnS75QwwkgQCc01HRB3SQOQDrAbqCW4D_3g&usqp=CAU'} className="App-logo" alt="logo" />
@@ -38,8 +92,18 @@ class App extends Component {
             Let's See Some weather!
           </p>
         </header>
-        <WeatherCard city = {'Tel-Aviv'}/>
-        <WeatherCard city = {'New-York'}/>
+        <WeatherCard city = { this.state.name1 }
+          temperature = {this.state.city1.temperature}
+          humidity = {this.state.city1.humidity}
+          windspeed = {this.state.city1.windspeed}
+          iconURL = {this.state.city1.icon}
+        />
+        <WeatherCard city = { this.state.name2 }
+          temperature = {this.state.city2.temperature}
+          humidity = {this.state.city2.humidity}
+          windspeed = {this.state.city2.windspeed}
+          iconURL = {this.state.city2.icon}
+        />
       </div>
     );
   }
--- src/Components/WeatherCard/WeatherCard.js
+++ src/Components/WeatherCard/WeatherCard.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import './WeatherCard.css';
 
-const WeatherCard = ( {city }) => {
+const WeatherCard = ( {city, temperature, humidity, windspeed, iconURL}) => {
+  console.log(iconURL)
   return (
     <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
       <main className="pa4 black-80">
@@ -9,10 +10,16 @@ const WeatherCard = ( {city }) => {
           <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
             <legend className="f1 fw6 ph0 mh0">{city}</legend>
             <div className="mt3">
-              <label className="db fw6 lh-copy f6">Temperature</label>
-              <label className="db fw6 lh-copy f6">Humidity</label>
-              <label className="db fw6 lh-copy f6">Windspeed</label>
-              <label className="db fw6 lh-copy f6">Icon</label>
+              <label className="db fw6 lh-copy f6">
+                {`Temprature: ${temperature} °C`}
+              </label>
+              <label className="db fw6 lh-copy f6">
+                {`Humidity: ${humidity}%`}
+              </label>
+              <label className="db fw6 lh-copy f6">
+                {`Windspeed: ${windspeed}Km/h`}
+              </label>
+              <img id='inputImg' alt='' src= { `https://${iconURL}` } width='64px' hight='64px' />
             </div>
           </fieldset>
         </div>
