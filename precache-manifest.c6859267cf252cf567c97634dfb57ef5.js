1ca883e3393f746f014fb7b2cce59b3297f4f8f5
Yogev Henig <yogevhenig@gmail.com>
2020-09-23 13:28:09 +0300

    Updates

---
+++ precache-manifest.c6859267cf252cf567c97634dfb57ef5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81ba6605ff749a43f95b81c1a30488b3",
+    "url": "/yogevHenig/Weather/index.html"
+  },
+  {
+    "revision": "40419ca4e6b05b4d78cc",
+    "url": "/yogevHenig/Weather/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "3630f71c5087a10dd396",
+    "url": "/yogevHenig/Weather/static/css/main.6dd62ab5.chunk.css"
+  },
+  {
+    "revision": "40419ca4e6b05b4d78cc",
+    "url": "/yogevHenig/Weather/static/js/2.42eb1926.chunk.js"
+  },
+  {
+    "revision": "e6191615acbaa88870aef4cd0368e4ec",
+    "url": "/yogevHenig/Weather/static/js/2.42eb1926.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3630f71c5087a10dd396",
+    "url": "/yogevHenig/Weather/static/js/main.41941928.chunk.js"
+  },
+  {
+    "revision": "2c6d7907a1d764aa315a",
+    "url": "/yogevHenig/Weather/static/js/runtime-main.142c0042.js"
+  }
+]);
