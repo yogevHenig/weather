c3529b0a31e5a6662bf4b6b65ea4ed226ea110f6
Yogev Henig <yogevhenig@gmail.com>
2020-09-24 07:42:28 +0300

    Updates

---
+++ precache-manifest.4e3c3abd2f453eb9300c3660919bbeb2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e40e93676c2e77891ff72a0e4122554",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "6226cd51421ddf759370",
+    "url": "/weather/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "02c36bf06278232ccfb2",
+    "url": "/weather/static/css/main.bedf184b.chunk.css"
+  },
+  {
+    "revision": "6226cd51421ddf759370",
+    "url": "/weather/static/js/2.ee886d12.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather/static/js/2.ee886d12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02c36bf06278232ccfb2",
+    "url": "/weather/static/js/main.aeecce60.chunk.js"
+  },
+  {
+    "revision": "65a135568c88d392bf25",
+    "url": "/weather/static/js/runtime-main.d8725fa2.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/weather/precache-manifest.299ec18c70dd7dc92a8563f00c6fdea8.js"
+  "/weather/precache-manifest.4e3c3abd2f453eb9300c3660919bbeb2.js"
 );
 
 self.addEventListener('message', (event) => {
